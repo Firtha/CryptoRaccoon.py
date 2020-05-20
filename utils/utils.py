bf6b868067acc12091fd59401e58ab7b86fe4e22
max <maxime.cpro@outlook.fr>
2020-05-20 16:33:37 +0200

    first try of a game with a commande menu and updating some icon. Init db as json files in src/ and adding a utils folder

---
+++ utils/utils.py
@@ -0,0 +1,7 @@
+import pygame, sys
+from pygame.locals import *
+
+#Print rectangle x,y, center data
+print(f'x={button_1.x}, y={button_1.y}, w={button_1.w}, h={button_1.h}')
+print(f'left={button_1.left}, top={button_1.top}, right={button_1.right}, bottom={button_1.bottom}')
+print(f'center={button_1.center}')
